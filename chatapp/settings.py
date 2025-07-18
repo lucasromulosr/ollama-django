8fe362cd425a1b5097efa215636fb58e689c3f63
lucasromulosr <lucas.romulo.sr@gmail.com>
2025-07-18 13:37:34 -0300

    feat: add ollama_app

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'chat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chat',
+    'ollama_api'
 ]
 
 MIDDLEWARE = [
---
+++ ollama_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OllamaApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ollama_api'
